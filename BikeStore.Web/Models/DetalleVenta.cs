ca9c4cea12bb721bebe333a6d5910bf44d199b5b
JoséCharco <jvchcuenca08@gmail.com>
2026-08-24 18:44:02 -0500

    Registor de ventas(sin API) y Cliente

---
+++ BikeStore.Web/Models/DetalleVenta.cs
@@ -0,0 +1,19 @@
+﻿namespace BikeStore.Models
+{
+    public class DetalleVenta
+    {
+        public int IdDetalle { get; set; }
+
+        public int IdVenta { get; set; }
+
+        public int IdBicicleta { get; set; }
+
+        public string Bicicleta { get; set; } = string.Empty;
+
+        public int Cantidad { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public decimal Subtotal { get; set; }
+    }
+}
